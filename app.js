1aed8ae0bec6e1b63beac99b4fa49f08b5ed436a
Ashok N <66304695+ashokide@users.noreply.github.com>
2021-08-28 11:04:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const editor = () => {
+    let html = document.querySelector("#htmleditor").innerText
+    let css = document.querySelector("#csseditor").innerText
+    let js = document.querySelector("#jseditor").innerText
+    let preview = document.querySelector(".preview")
+    let content = ""
+    content += `<style>${css}</style>`
+    content += `<script>${js}</script>`
+    content += `<body>${html}</body>`
+    preview.src = `data:text/html;charset=UTF-8,<html>${content}</html>`
+}
